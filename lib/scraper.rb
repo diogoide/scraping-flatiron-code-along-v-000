8bd1b24b517cb524b4d725c715a1a1ca1a87d57a
Diogo Ide <diogoide@gmail.com>
2018-06-09 18:04:27 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,10 +23,10 @@ class Scraper
         course.description = post.css("p").text
       end
     end
-Scraper.new.print_courses
+
 end
 
-
+Scraper.new.print_courses
 
   #   def print_courses
   #   self.make_courses
